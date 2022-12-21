7b927f5895ad0dec523c088eb4869379da8794ec
NUZ1740 <butheleziyah@gmail.com>
2022-12-21 01:00:51 -0800

    size is not granduer

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
-#include  <stdio.h>
+#include <stdio.h>
+
 /**
- * main - Entry
- * Return: Always 0
+ * main - Entry point
+ * Return: Always return 0 (success)
  */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
-	return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+
 }
