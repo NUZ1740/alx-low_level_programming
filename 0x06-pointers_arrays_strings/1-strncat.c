03ec61efb4ed60745ad1dd93e1eea7df1ffcd24c
NUZ1740 <butheleziyah.com>
2023-01-02 01:56:36 -0800

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,9 +18,9 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 	}
 	j = 0;
-	while (j < n && src[j]  != '\0')
+	while (j < n && src[j] != '\0')
 	{
-		dest[i] = dest[j];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
